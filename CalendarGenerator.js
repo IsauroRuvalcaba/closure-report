8e3017b8a3a920f78686c220e9b24b817e59df3a
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2026-05-08 12:29:28 -0700

    refactor getDateInfo() method with a more simpler approach. per g.g.

--- CalendarGenerator.js
+++ CalendarGenerator.js
@@ -4,9 +4,7 @@ export default class CaledarGenerator {
     this.month = month;
     this.currMDate = new Date(this.year, this.month - 1, 1);
     this.startCol = this.currMDate.getDay();
-    this.monthLength = new Date(
-      this.currMDate.setMonth(this.currMDate.getMonth() + 1, 0),
-    ).getDate();
+    this.monthLength = new Date(this.year, this.month, 0).getDate();
     this.prevMDate = new Date(this.year, this.month - 1, 0).getDate();
     this.calGrid = new Array(6 * 7).fill(null);
     this.calSetup();
@@ -16,52 +14,34 @@ export default class CaledarGenerator {
     console.log(this.calGrid);
   }
 
-  // getDateInfo(dateVal, direction) {
-  //   const dateValue =
-  //     dateVal === "month"
-  //       ? getMonth()
-  //       : dateVal === "year"
-  //         ? getFullYear()
-  //         : null;
-  //   const direct = direction === "prev" ? -1 : direction === "post" ? 1 : null;
-
-  //   return new Date(this.currMDate.setMonth(this.currMDate.getMonth() + direct))
-  //     .dateValue;
-  // }
-
   getDateInfo(dateVal, direction) {
-    // choose a getter function that accepts a Date and returns the requested value
-    const getter =
-      dateVal === "month"
-        ? (d) => d.getMonth()
-        : dateVal === "year"
-          ? (d) => d.getFullYear()
-          : null;
+    if (direction === "prev") {
+      if (dateVal === "year") {
+        return this.month === 1 ? this.year - 1 : this.year;
+      } else if (dateVal === "month") {
+        return this.month === 1 ? 12 : this.month - 1;
+      }
+    }
 
-    // compute direction: prev -> -1, post -> +1, otherwise 0
-    const direct = direction === "prev" ? -1 : direction === "post" ? 1 : 0;
-
-    if (!getter) return null;
-
-    // const fixedDate = new Date(this.year, this.month - 1, 1);
-    // clone current month date so we don't mutate original unintentionally
-    // const base = new Date(this.currMDate.getTime());
-    const base = new Date(this.year, this.month - 1, 1);
-
-    // shift month (setMonth handles month overflow)
-    base.setMonth(base.getMonth() + direct);
-
-    // return the requested value from the adjusted date
-    return getter(base);
+    if (direction === "post") {
+      if (dateVal === "year") {
+        return this.month === 12 ? this.year + 1 : this.year;
+      } else if (dateVal === "month") {
+        return this.month === 12 ? 1 : this.month + 1;
+      } else {
+        return null;
+      }
+    }
   }
 
   calSetup() {
     let preMDayStart = this.prevMDate - (this.startCol - 1);
     const totPreCurDays = this.startCol + this.monthLength;
+
     const prevMoYear = this.getDateInfo("year", "prev");
+    const prevMoMo = this.getDateInfo("month", "prev");
     const nextMoYear = this.getDateInfo("year", "post");
-    const prevMoMo = this.getDateInfo("month", "prev") + 1;
-    const nextMoMo = this.getDateInfo("month", "post") + 1;
+    const nextMoMo = this.getDateInfo("month", "post");
 
     let endBuffer = 0;
 
@@ -76,7 +56,6 @@ export default class CaledarGenerator {
     }
 
     for (let i = this.startCol; i < this.monthLength + this.startCol; i++) {
-      // this.calGrid[i] = i - this.startCol + 1;
       this.calGrid[i] = {
         dayNumber: i - this.startCol + 1,
         isCurrentMonth: true,
@@ -86,7 +65,6 @@ export default class CaledarGenerator {
 
     for (let i = totPreCurDays; i < 42; i++) {
       endBuffer++;
-      // this.calGrid[i] = endBuffer;
       this.calGrid[i] = {
         dayNumber: endBuffer,
         isCurrentMonth: false,
